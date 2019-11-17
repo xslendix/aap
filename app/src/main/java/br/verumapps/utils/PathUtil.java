307cdc0a52c35b0e037450a84557ab2a4b0fee3e
gxlden <thiagocabrr@gmail.com>
2019-11-17 19:42:35 -0300

    Changed things in PathUtil.java (testing)

--- app/src/main/java/br/verumapps/utils/PathUtil.java
+++ app/src/main/java/br/verumapps/utils/PathUtil.java
@@ -8,6 +8,12 @@ public class PathUtil
 
     public  boolean isAndroidProject (String path)
     {
+		File p = new File(path + "/AndroidManifest.xml");
+		File x = new File(path + "/res/");
+		
+		if (p.exists() && x.exists()) return true;
+		
+		
         if (true)
         {
             File dir = new File(path + "/app");
@@ -33,15 +39,31 @@ public class PathUtil
     }
     public  String getAssetsFolder (String path)
     {
-        return path;
+		File x = new File(path + "/assets/");
+
+		if ( x.exists()) return path + "/assets/";
+        File dir = new File(path + "/app/src/main/assets/");
+		if(dir.exists()) return path + "/app/src/main/assets/";
+		return "false";
     }
 
     public  String getResFolder (String path)
     {
-        return path;
+		
+        File x = new File(path + "/res/");
+
+		if ( x.exists()) return path + "/res/";
+        File dir = new File(path + "/app/src/main/res/");
+		if(dir.exists()) return path + "/app/src/main/res/";
+		return "false";
     }
     public  String getJavaFolder (String path)
     {
-        return path;
+        File x = new File(path + "/java/");
+
+		if ( x.exists()) return path + "/java/";
+        File dir = new File(path + "/app/src/main/java/");
+		if(dir.exists()) return path + "/app/src/main/java/";
+		return "false";
     }
 }
