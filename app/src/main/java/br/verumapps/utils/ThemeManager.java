d94a8acd944eda3433214b4388572181e281e599
dxffety <daffetyxd@gmail.com>
2019-11-18 19:19:56 -0300

    Some changes

--- app/src/main/java/br/verumapps/utils/ThemeManager.java
+++ app/src/main/java/br/verumapps/utils/ThemeManager.java
@@ -7,31 +7,38 @@ import android.widget.Toast;
 public class ThemeManager
 {
     private Context ctx;
-    
-    public ThemeManager(Context context) {
+
+    public ThemeManager (Context context)
+    {
         ctx = context;
     }
-    
-    public void setActivityTheme(String name) {
+
+    public void setActivityTheme (String name)
+    {
         // i will fix later
         int id;
-        
-        switch (name) {
+
+        switch (name)
+        {
             case "Strawberry": id = R.style.strawberry; break;
             case "Light": id = R.style.light; break;
             default: id = R.style.Theme; break;
         }
-        
+
         // for som reason it is null
         ctx.setTheme(id);
-        
     }
-    
-    public int getDialogTheme(String name) {
-        switch (name) {
-            case "Strawberry": return R.style.StrawberryDialog;
-            case "Light": return R.style.LightDialog;
-            default: return R.style.DarkDialog;
+
+    public int getDialogTheme (String name)
+    {
+        switch (name)
+        {
+            case "Strawberry":
+                return R.style.StrawberryDialog;
+            case "Light":
+                return R.style.LightDialog;
+            default:
+                return R.style.DarkDialog;
         }
     }
 }
